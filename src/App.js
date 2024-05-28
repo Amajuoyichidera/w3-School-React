666ecc93c24c8df98b305b543fd00294912f6aba
Amajuoyichidera <amajuoyichideradavid@gmail.com>
2024-05-28 19:08:18 +0100

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const myArray = [ 'apple', 'paw-paw', 'orange', 'banana', 'cucumber'];
+  function calculate(param) {
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {myArray.map((item) => 
+        <p>{item}</p>
+       )}
+      <h1>Hello World!</h1>
+      <p>Time to code</p>
     </div>
   );
 }
